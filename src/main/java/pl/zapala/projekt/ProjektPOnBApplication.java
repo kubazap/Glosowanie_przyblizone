68d97a4fd00c28842d13b2e909461269f9960f92
kubazap <jz.jakub.zapala@gmail.com>
2025-12-15 17:28:12 +0100

    Szkielet aplikacji

---
+++ src/main/java/pl/zapala/projekt/ProjektPOnBApplication.java
@@ -0,0 +1,13 @@
+package pl.zapala.projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektPOnBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektPOnBApplication.class, args);
+	}
+
+}
